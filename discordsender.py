631baa16d5453760534a38bff95c4670869bc96e
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-03-06 14:29:34 +0000

    tidied discordsender

--- discordsender.py
+++ discordsender.py
@@ -3,14 +3,15 @@ import config
 
 def sendDiscord(toSend, hookUrl):
 	def sendDiscordPart(partToSend):
-		url = hookUrl
-		data = {}
-		data['username'] = 'SwiftHua'
-		data['content'] = partToSend
-		requests.post(url, json = data, headers = {'Content-Type': 'application/json'}, timeout = 10)
+		jsonData = {}
+		jsonData['username'] = 'SwiftHua'
+		jsonData['content'] = partToSend
+		jsonData['allowed_mentions'] = {'parse': []}
 
-	toSend = str(toSend)
-	toSend = toSend.replace('@', '[AT]')
-	
+		try:
+			requests.post(hookUrl, json = jsonData, headers = {'Content-Type': 'application/json'}, timeout = 10)
+		except requests.exceptions.RequestException as e:
+			print(f'error sending to discord: {e}')
+
 	for i in range(int(len(toSend) / 2000) + 1):
 		sendDiscordPart(toSend[i * 2000:i* 2000 + 2000])
